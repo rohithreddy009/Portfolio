1166185229463ca686483fec3aad4fd20005c4c4
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-04-07 14:20:34 +0530

    revamp portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
           Projects
         </NavLink>
         {/* Replace NavLink for Resume with an <a> tag */}
-        <a href="https://docs.google.com/document/d/18LxCnOjm5Ltj79RXTV_nZ65ukfdII-rOaBpeosWtCYs/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="text-black hover:text-blue-600">
+        <a href="https://docs.google.com/document/d/1X_LntxJO1ut_oTyLuw7MfRMH6CqgfizwUFs6ibvQT78/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="text-black hover:text-blue-600">
           Resume
         </a>
       </nav>
