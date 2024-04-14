2a94b4b8dc3b5f0f6eecb9f657d91c47388d3ffa
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-04-14 15:15:40 +0530

    improvements

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -12,49 +12,49 @@ const HomeInfo = ({ currentStage }) => {
       </h1>
     );
 
-    if (currentStage === 2) {
-      return (
-        <div className='info-box'>
-          <p className='font-medium sm:text-xl text-center'>
-            Worked with multiple techstacks <br /> and picked up many skills along the way
-          </p>
+  //   if (currentStage === 2) {
+  //     return (
+  //       <div className='info-box'>
+  //         <p className='font-medium sm:text-xl text-center'>
+  //           Worked with multiple techstacks <br /> and picked up many skills along the way
+  //         </p>
   
-          <Link to='/about' className='neo-brutalism-white neo-btn'>
-            Learn more
-            <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-          </Link>
-        </div>
-      );
-    }
-  if (currentStage === 3) {
-    return (
-      <div className='info-box'>
-        <p className='font-medium text-center sm:text-xl'>
-          Built multiple projects over the period <br /> Check them out!
-        </p>
+  //         <Link to='/about' className='neo-brutalism-white neo-btn'>
+  //           Learn more
+  //           <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+  //         </Link>
+  //       </div>
+  //     );
+  //   }
+  // if (currentStage === 3) {
+  //   return (
+  //     <div className='info-box'>
+  //       <p className='font-medium text-center sm:text-xl'>
+  //         Built multiple projects over the period <br /> Check them out!
+  //       </p>
 
-        <Link to='/projects' className='neo-brutalism-white neo-btn'>
-          Visit my portfolio
-          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-        </Link>
-      </div>
-    );
-  }
+  //       <Link to='/projects' className='neo-brutalism-white neo-btn'>
+  //         Visit my portfolio
+  //         <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+  //       </Link>
+  //     </div>
+  //   );
+  // }
 
-  if (currentStage === 4) {
-    return (
-      <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-        Need a project done or looking for a dev? <br/> I'm just a few keystrokes away
-      </p>
+  // if (currentStage === 4) {
+  //   return (
+  //     <div className='info-box'>
+  //     <p className='font-medium sm:text-xl text-center'>
+  //       Need a project done or looking for a dev? <br/> I'm just a few keystrokes away
+  //     </p>
 
-      <Link to='/contact' className='neo-brutalism-white neo-btn'>
-        Let's talk
-        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-      </Link>
-    </div>
-    );
-  }
+  //     <Link to='/contact' className='neo-brutalism-white neo-btn'>
+  //       Let's talk
+  //       <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+  //     </Link>
+  //   </div>
+  //   );
+  // }
 
   return null;
 };
